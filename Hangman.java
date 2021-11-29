89f6d5e86bd9e9d7414ca449c56ba213942f5b54
abuka <abukavictoro@gmai.com>
2021-11-29 12:42:25 +0100

    Initial Commit

---
+++ Hangman.java
@@ -0,0 +1,29 @@
+package HangmanPack.Hangman;
+
+
+/*
+ *
+ * Anything the user inputs, say how many consonants and vowels are there
+ * First input should be a character 
+ * After getting the first input accept a word from the user and 
+ * say how many consonants and vowels are in the word
+ * 
+ */
+
+public class Hangman {
+	static Object age = 12;
+	
+//	Personal Taks:
+//	Create a slack org and call it Java
+//	Create a github repo for the Hangman task
+	
+//	(Hangman Game) Task 1(Row 1):
+//	Use the length of the words to display the dashes
+	
+//	Task 2:
+//	Check if the character exists in the word
+	
+	public static void main(String[] args) {
+		System.out.println(age.getClass());
+	}
+}
