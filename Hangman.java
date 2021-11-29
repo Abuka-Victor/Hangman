01fc841b7d7e94c26f493da628ea32a027e8e59c
abuka <abukavictoro@gmai.com>
2021-11-29 13:19:19 +0100

    Distribution Code

--- Hangman.java
+++ Hangman.java
@@ -2,25 +2,56 @@ package HangmanPack.Hangman;
 
 
 /*
- *
- * Anything the user inputs, say how many consonants and vowels are there
- * First input should be a character 
- * After getting the first input accept a word from the user and 
- * say how many consonants and vowels are in the word
- * 
+ * Problem Statement: In the Hangman game, when a user decides to play the Hangman game, 
+ * the numbers of dashes or blanks that are equal to the length of a word are displayed. 
+ * To implement this, Peter needs to store a group of words, randomly pick a word, 
+ * calculate the length of the word, and finally displays the number of dashes or blanks. 
+ * In addition, he wants to implement the functionality that takes an alphabetical character 
+ * as an input from the user, check if the character entered by the user exists in the 
+ * corresponding word, and display the letter in the appropriate dashes. 
+ * However, if the character doesn’t exist in the word, it is added to the list of missed letters.
+ *  Help Peter to achieve the preceding requirement.
+ *  
+ *  
+ *  Task 1(Row 1):
+ *  Use the length of the words to display the dashes
+ *  
+ *  Task 2(Row 1):
+ *  Check if the character exists in the word
+ *  
+ *  Task 3(Row 2):
+ *  Implement the functionality that takes an alphabetical character as an input from the user
+ *  
+ *  Task 4(Row 3):
+ *  Storing the words inside an array and randomly picking a word
+ *  
  */
 
+// Necessary Imports Go Here:
+import java.util.Scanner;
+// Imports End Here.
+
 public class Hangman {
-	static Object age = 12;
+	//	Define Variables Here
 	
-	
-//	(Hangman Game) Task 1(Row 1):
-//	Use the length of the words to display the dashes
-	
-//	Task 2:
-//	Check if the character exists in the word
 	
 	public static void main(String[] args) {
-		System.out.println(age.getClass());
+		
+	}
+	
+	void displayBlanks(){
+		//TODO: Row 1
+	}
+	
+	void userInput() {
+		//TODO: Row 2
+	}
+	
+	void validateInput() {
+		//TODO: Row 1
+	}
+	
+	void randomPick() {
+		//TODO: Row 3
 	}
 }
